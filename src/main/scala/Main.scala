c42528264ee7ffc17b576b91f7745a0940f47c31
yassu <yassu0320.dev@gmail.com>
2018-08-23 02:16:16 +0900

    Update Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,46 @@ package projecteuler
 import projecteuler.exercises._
 
 object ProjectEuler extends App {
-  println(Ex013.run())
+  private val EXERCISES = Seq(
+    Ex001,
+    Ex002,
+    Ex003,
+    Ex004,
+    Ex005,
+    Ex006,
+    Ex007,
+    Ex008,
+    Ex009,
+    Ex010,
+    Ex012,
+    Ex013,
+    Ex015,
+    Ex016,
+    Ex039,
+    Ex040,
+    Ex041,
+    Ex042,
+  )
+
+  private def runAll() = {
+    val start = System.currentTimeMillis
+    EXERCISES.foreach(e => {
+      runOne(e)
+      println("=" * 80)
+    })
+    println("Total:" + (System.currentTimeMillis - start) / 1000.0 + " sec")
+  }
+
+  private def runOne(ans: Answer) = {
+    val numberString =
+      """Ex\d+""".r.findAllIn(ans.getClass.toString).matchData.toSeq.head.group(0)
+    println("Problem: " + numberString)
+
+    val start = System.currentTimeMillis
+    println("Answer: " + ans.run.toString)
+    println("Spend " + (System.currentTimeMillis - start) / 1000.0 + " sec")
+  }
+
+  runAll()
 }
 
